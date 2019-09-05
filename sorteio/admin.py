604bf7f4c53951e83f4d066b46363928f6b861e2
Rayan Sóstenes <rayancocos@gmail.com>
2019-09-05 05:37:09 -0300

    Add sorteio and usuarios models

--- sorteio/admin.py
+++ sorteio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Sorteio, Pessoa
 # Register your models here.
+
+@admin.register(Sorteio)
+class SorteioAdmin(admin.ModelAdmin):
+    list_display = ['premio', 'post_instagram', 'perfil_instagram']
+
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ['qr_code', 'nome', 'perfil_instagram', 'telefone']
--- sorteio/models.py
+++ sorteio/models.py
@@ -1,7 +1,7 @@
 import uuid
 from django.db import models
 
-class BaseModel(models.Model, FieldsChangedMixin):
+class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -9,4 +9,14 @@ class BaseModel(models.Model, FieldsChangedMixin):
     class Meta:
         abstract = True
 
-class Sorteio
+class Pessoa(BaseModel):
+    qr_code = models.CharField(max_length=255, unique=True)
+    nome = models.CharField(max_length=255)
+    perfil_instagram = models.CharField(max_length=255, blank=True, null=True)
+    telefone = models.CharField(max_length=255, blank=True, null=True)
+
+class Sorteio(BaseModel):
+    premio = models.CharField(max_length=1024)
+    post_instagram = models.CharField(max_length=1024)
+    perfil_instagram = models.CharField(max_length=1024)
+    inscritos = models.ManyToManyField(Pessoa)
